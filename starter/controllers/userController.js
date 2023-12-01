148ff506eec2571206667d957c244914ada84857
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-01 12:46:57 +0530

    Update userController.js

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -1,13 +1,18 @@
-
-
-
-
+const User=require('../models/user')
+const {StatusCodes}=require('http-status-codes')
+const CustomError=require('../errors/index')
 const getAllUsers=async (req,res)=>{
-    res.send('get all user')
+    const users=await User.find({role:'user'}).select('-password')//we remove passwor dand return everyotehr details
+    res.status(StatusCodes.OK).json({users})
 }
 
 const getSingleUser=async (req,res)=>{
-    res.send('get single user')
+    const {id}=req.params
+    const user=await User.findOne({_id:id}).select('-password')
+    if(!user){
+        throw new CustomError.NotFoundError('no user with id')
+    }
+    res.status(StatusCodes.OK).json({user})
 }
 
 const showCurrentUser=async (req,res)=>{
