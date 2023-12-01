3c3f763e9da231250fa90aea9ced3f939269b902
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-01 13:23:06 +0530

    Update authentication.js

--- starter/middleware/authentication.js
+++ starter/middleware/authentication.js
@@ -3,11 +3,17 @@ const {isTokenValid}=require('../utils/index')
 
 const authenticateUser=async (req,res,next)=>{
     const token=req.signedCookies.token
-    const isValid=isTokenValid({token:token})
-    if(!isValid){
+    if(!token){
         throw new CustomError.UnauthenticatedError('no permission')
     }
+    try{
+    const {name,userId,role}=isTokenValid({token:token})
+    req.user={name,userId,role}
     next();
+    }
+    catch(err){
+        throw new CustomError.UnauthenticatedError('no permission')
+    } 
 }
 module.exports={
     authenticateUser
