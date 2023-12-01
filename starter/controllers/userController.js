85aa6ee85b67af129b8628587f5501fd018f71aa
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-01 17:51:34 +0530

    worked on updatepassword controller

--- starter/controllers/userController.js
+++ starter/controllers/userController.js
@@ -1,6 +1,8 @@
 const User=require('../models/user')
 const {StatusCodes}=require('http-status-codes')
 const CustomError=require('../errors/index')
+
+
 const getAllUsers=async (req,res)=>{
     const users=await User.find({role:'user'}).select('-password')//we remove passwor dand return everyotehr details
     res.status(StatusCodes.OK).json({users})
@@ -24,7 +26,20 @@ const updateUser=async (req,res)=>{
 }
 
 const updateUserPassword=async (req,res)=>{
-    res.send('update user')
+    const {oldPassword,newPassword}=req.body
+    if(!oldPassword || !newPassword){
+        throw new CustomError.BadRequestError('provide details')
+    }
+    const user=await User.findOne({_id:req.user.userId})
+    if(!user){
+        throw new CustomError.UnauthenticatedError('user not found')
+    }
+    const isMatch=await user.comparePassword(oldPassword)
+    if(!isMatch){
+        throw new CustomError.UnauthenticatedError('invalid credentials')
+    }
+    user.password=newPassword
+    await user.save()//instead of updating we can use save method
 }
 
 
--- starter/routes/userRoutes.js
+++ starter/routes/userRoutes.js
@@ -7,8 +7,8 @@ const {authenticateUser,authorizePermissions}=require('../middleware/authenticat
 router.route('/').get(authenticateUser,authorizePermissions('admin','owner'),getAllUsers)
 
 router.route('/showMe').get(authenticateUser,showCurrentUser)//this should be setup above :id route if :id route is before then that 'showMe' would be treated as id then returns error
-router.route('/updateUser').patch(updateUser)
-router.route('/updateUserPassword').patch(updateUserPassword)
+router.route('/updateUser').patch(authenticateUser,updateUser)
+router.route('/updateUserPassword').patch(authenticateUser,updateUserPassword)
 
 router.route('/:id').get(authenticateUser,getSingleUser)
 
