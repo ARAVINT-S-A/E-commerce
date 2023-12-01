b17fa69f577ae3dcca858a4b98ddd8b19322d3b3
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-01 12:44:06 +0530

    added

--- starter/routes/userRoutes.js
+++ starter/routes/userRoutes.js
@@ -6,8 +6,8 @@ const{getAllUsers,getSingleUser,showCurrentUser,updateUser,updateUserPassword}=r
 router.route('/').get(getAllUsers)
 
 router.route('/showMe').get(showCurrentUser)//this should be setup above :id route if :id route is before then that 'showMe' would be treated as id then returns error
-router.route('/updateUser').post(updateUser)
-router.route('/updateUserPassword').post(updateUserPassword)
+router.route('/updateUser').patch(updateUser)
+router.route('/updateUserPassword').patch(updateUserPassword)
 
 router.route('/:id').get(getSingleUser)
 
