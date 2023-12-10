4393a6462c7a57697ec234733ccf50aec43e8900
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-10 13:21:33 +0530

    added funtionality

--- starter/controllers/productController.js
+++ starter/controllers/productController.js
@@ -1,13 +1,21 @@
 const CustomError=require('../errors/index')
 const Product=require('../models/products')
+const {StatusCodes}=require('http-status-codes')
+
 const createProduct=async(req,res)=>{
-    res.send('create')
+    req.body.user=req.user.userId
+    const product=await Product.create(req.body)
+    res.status(StatusCodes.CREATED).json({product})
 }
 const getAllProducts=async (req,res)=>{
-    res.send('get all products')
+    const products=await Product.find({});
+    res.status(StatusCodes.OK).json({products})
 }
 const getSingleProducts=async (req,res)=>{
     res.send('get single products')
+}
+const updateProduct=async (req,res)=>{
+    res.send('update')
 }
 const deleteProduct=async (req,res)=>{
     res.send('delete')
@@ -21,6 +29,7 @@ module.exports={
     createProduct,
     getAllProducts,
     getSingleProducts,
+    updateProduct,
     deleteProduct,
     uploadImage
 }
--- starter/routes/productRoutes.js
+++ starter/routes/productRoutes.js
@@ -1,11 +1,11 @@
 const express=require('express')
 const router=express.Router()
 
-const {createProduct,getAllProducts,getSingleProducts,deleteProduct,uploadImage}=require('../controllers/productController')
+const {createProduct,getAllProducts,getSingleProducts,deleteProduct,uploadImage,updateProduct}=require('../controllers/productController')
 const { authenticateUser, authorizePermissions } = require('../middleware/authentication')
 
 router.route('/').post(authenticateUser,authorizePermissions("admin"),createProduct).get(getAllProducts)
 router.route('/uploadimage').post(authenticateUser,authorizePermissions('admin'),uploadImage)
-router.route('/:id').patch(authenticateUser,authorizePermissions('admin'),deleteProduct).post(getSingleProducts)
+router.route('/:id').delete(authenticateUser,authorizePermissions('admin'),deleteProduct).patch(authenticateUser,authorizePermissions('admin'),updateProduct).get(getSingleProducts)
 
 module.exports=router
