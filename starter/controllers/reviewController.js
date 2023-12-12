6befab6ba254c9bd7df3fd6ebc45544b00557f89
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-12 12:32:32 +0530

    Update reviewController.js

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -44,10 +44,17 @@ const getSingleReview=async(req,res)=>{
 const updateReview=async(req,res)=>{
     const {id}=req.params
     const{rating,title,comment}=req.body
-    const review =await Review.findOneAndUpdate({_id:id},{rating,title,comment},{new:true,runValidators:true})
+    const review =await Review.findOne({_id:id})
+    
     if(!review){
         throw new CustomError.NotFoundError('no review with id')
     }
+
+    checkPermissions(req.user,review.user)
+    review.rating=rating
+    review.title=title
+    review.comment=comment
+    review.save()
     res.status(StatusCodes.OK).json({review})
 }
 
@@ -57,6 +64,7 @@ const deleteReview=async(req,res)=>{
     if(!review){
         throw new CustomError.NotFoundError('no product found')
     }
+    checkPermissions(req.user,review.user)
     await review.remove()
     res.status(StatusCodes.OK).json('review deleted')
 }
