2706d5001345b5bcec90146e5c550202f3b1e4ed
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-12 11:46:37 +0530

    Update reviewController.js

--- starter/controllers/reviewController.js
+++ starter/controllers/reviewController.js
@@ -1,29 +1,44 @@
 const CustomError=require('../errors/index')
 const{StatusCodes}=require('http-status-codes')
 const Review=require('../models/reviews')
+const Product=require('../models/products')
+const {checkPermissions}=require('../utils/index')
 
 const createReview=async(req,res)=>{
-    res.send('create review');
-    // if(!req.body){
-    //     throw new CustomError.BadRequestError('no review details found')
-    // }
-    // const review=await Review.create(...req.body)
-    // if(!review){
-    //     throw new CustomError
-    // }
-    // res.status(StatusCodes.CREATED).json({review}) 
+    const {product:productId}=req.body
+    const isValidProduct =await Product.findOne({_id:productId})
+    
+    if(!isValidProduct){
+        throw new CustomError.NotFoundError('no product found')
+    }
+
+    const alreadyreviewed=await Review.findOne({user:req.user.userId,product:productId})
+    
+    if(alreadyreviewed){
+        throw new CustomError.BadRequestError('already reviewed')
+    }
+
+    req.body.user=req.user.userId
+
+    const review=await Review.create(req.body)
+
+    res.status(StatusCodes.CREATED).json({review}) 
 }
 
 const getAllReviews=async(req,res)=>{
-    res.send('get all reviews')
-    // const reviews=await Review.find({product:productId})
-    // if(!reviews){
-    //     throw new CustomError.NotFoundError('no product')
-    // }
-    // res.status(StatusCodes.OK).json({reviews})
+    const reviews=await Review.find({})
+    if(!reviews){
+        throw new CustomError.NotFoundError('no reviews')
+    }
+    res.status(StatusCodes.OK).json({reviews})
 }
 const getSingleReview=async(req,res)=>{
-    res.send('get single review')
+    const{id}=req.params;
+    const review=await Review.findOne({_id:id})
+    if(!review){
+        throw new CustomError.NotFoundError('no review with id')
+    }
+    res.status(StatusCodes.OK).json({review})
 }
 
 const updateReview=async(req,res)=>{
