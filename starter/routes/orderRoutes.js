c4ed915e533c53f06dafe6812a445362775318e7
zero <116646226+ARAVINT-S-A@users.noreply.github.com>
2023-12-16 10:19:16 +0530

    Update orderRoutes.js

--- starter/routes/orderRoutes.js
+++ starter/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const {getAllOrders,getSingleOrder,getCurrentUserOrders,createOrder,updateOrder}= require('../controllers/orderController')
+const { authorizePermissions } = require('../middleware/authentication')
 const router=express.Router()
 
-router.route('/').get(getAllOrders).post(createOrder)
+router.route('/').get(authorizePermissions('admin'),getAllOrders).post(createOrder)
 router.route('/showAllMyOrders').get(getCurrentUserOrders)
 router.route('/:id').get(getSingleOrder).patch(updateOrder)
 
